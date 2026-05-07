556281d94401dd7cdd5cbe059d05deae16b9b8e6
iamgokilavani-lang <iamgokilavani@gmail.com>
2026-05-07 23:04:01 +0530

    Add files via upload

---
+++ wed 07 may/script.js
@@ -0,0 +1,262 @@
+//section 1:
+//1. what is the output?
+
+//var a=10;
+//a=15;
+//console.log(a);
+
+//output: 15
+      
+
+//2.what happens here?
+
+//let b=20;
+//let b=30;
+   // :Identifier 'b' has already been declared
+
+
+//3.what is the output?
+
+     //const c=50;
+     //console.log(c);
+
+     //output:50
+
+//4.identify error
+    
+   // const x=10;
+   // x=20;
+
+   // ans: Assignment to constant variable.
+
+   //5.output?
+  // var p=5;
+   //var p=10;
+   //console.log(p);
+
+   //output:10
+
+   
+   //6.output?
+   //let q=25;
+   //q=q+10;
+   //console.log(q);
+   
+   //output:30
+
+   //7.output?
+
+   //const r=7;
+   //console.log(r+3);
+
+   //output:10
+
+   //8.fill correct keyword:
+
+    // a = 10;
+
+    // output:var a = 10;
+
+    //section2
+
+   // 9.Which method prints normal output?
+   // ans:console.log("Hello");
+      // string
+     
+
+ // 10.Which method shows warning?
+     //ans:console.warn()
+
+   
+   //11.Which method shows error?
+   //console.error()
+   
+   
+   //12.What does this do?
+   //console.clear();--all message clear
+
+//section 3:
+//13.output?
+
+   //let a = "hello";
+//console.log(typeof a);
+
+//Output:string
+
+
+//14.let b = 100;
+
+//console.log(typeof b);
+
+//Output:
+
+//number
+
+
+//15.let c = false;
+//console.log(typeof c);
+
+//Output:
+
+//boolean
+
+//16.let d;
+//console.log(d);
+
+//Output:
+
+//undefined
+
+
+17.//console.log(typeof null);
+
+18.//let num = Number("25");
+//console.log(num);
+
+
+
+//section 4:
+19. //let fruits = ["apple", "banana", "mango"];
+//console.log(fruits);
+
+//Output:
+
+//["apple", "banana", "mango"]
+
+
+20.
+ 
+//let arr = ["a","b","c"];
+//console.log(arr[1]);
+
+//Output:b
+
+21.
+//let arr = ["x","y","z"];
+//console.log(arr[arr.length-1]);
+
+//Output:z
+
+22. 
+
+//let arr = ["apple","banana","mango"];
+//console.log(arr[0]);
+
+//Output:apple
+
+23.
+//let arr = ["apple","banana"];
+//console.log(arr.length);
+
+//Output:2
+ 
+
+24.
+//let arr = ["apple","banana"];arr.push("mango");console.log(arr);
+//Output:
+//["apple", "banana", "mango"]
+
+
+
+//section 5:
+25.
+  //let person = { name: "Naveen", age: 25};console.log(person);
+  //Output:{ name: 'Naveen', age: 25 }
+
+  26.
+    // let obj = {name:"Naveen"};
+//console.log(obj.name);
+
+//Output:Naveen
+
+
+27.
+//let obj = {
+  //fruits: ["apple","banana"]
+//};
+//console.log(obj.fruits[1]);
+
+Output:banana
+
+28.
+   //let obj = {
+  //fruits: ["apple","banana","orange"]
+//};
+
+//console.log(obj.fruits[obj.fruits.length - 1]);
+
+//Output:orange
+
+
+
+
+//section 6:
+ 29.
+   //console.log(5 + 3);
+
+//Output:8
+
+30.
+//console.log(10 % 3);
+
+//Output:1
+
+31.
+   //console.log(2 ** 3);
+
+//Output:8
+
+32.
+//console.log(10 / 2);
+
+//Output:5
+
+
+//section:7
+33.
+//let a = 5;
+//a++;
+//console.log(a);
+
+//Output:6
+
+34.
+//let b = 5;
+//let c = b++;
+//console.log(b, c);
+
+//Output:6,5
+
+35  
+//let b = 5;
+//let c = b++;
+//console.log(b, c);
+
+Output:6 ,5
+
+36.
+  //let m = 3;
+//let n = m--;
+//console.log(m, n);
+
+Output:2, 3
+
+
+
+//section:8
+37.
+//console.log(5 == "5");
+//Output:true
+
+38.
+//console.log(5 === "5");
+//Output:false
+
+39.
+//console.log(true && false || true);
+//Output:true
+
+//section:9
+40.
+//5 > 3 ? console.log("Yes") : console.log("No");
+//output:yes ,true
